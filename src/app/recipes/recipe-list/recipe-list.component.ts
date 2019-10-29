238640aa4d246889573aa145eaf4b2df830eecb3
Dirk Gietelink <dirkgietelink@gmail.com>
2019-10-29 18:23:02 +0100

    vid 52: Added hardcoded Recipe instance. Added hardcoded content to recipes list html.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe(
+      'Kip spek rollades met feta kaas',
+      'Heerlijk te bereiden. Rol de kip en feta in het spek. Bakken voor 15 minuten',
+      'https://cdn.pixabay.com/photo/2017/06/21/22/42/recipe-2428926_1280.jpg' )
+  ];
   constructor() { }
 
   ngOnInit() {
