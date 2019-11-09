2d11da9226819c85751b83457a17c37c08314686
Dirk Gietelink <dirkgietelink@gmail.com>
2019-11-09 18:47:56 +0100

    vid 85: Created navigation between recipe and shopping list based on EventEmitter<string feature> and *ngIf and @Output for propogating property to its parent.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   collapsed = true;
+  @Output() featureSeleceted = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(feature: string) {
+    this.featureSeleceted.emit(feature);
   }
 
 }
